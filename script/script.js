4aa288c37f49410c39ac69c671d7c002a9dc0bea
Oleh Rozhko <oleh.rozhko5@gmail.com>
2021-10-19 22:00:25 +0300

    Другий урок завершено

--- script/script.js
+++ script/script.js
@@ -1,9 +1,21 @@
-var money;
-var income;
-var addExpenses;
-var deposit;
-var mission;
-var period;
+/* jshint node: true */
+var money = 500; // Дохід за місяць
+var income = "Sport"; //Додатковий дохід
+var addExpenses = "Fitnes , Girls , Beer"; // Додаткові витрати
+var deposit = true;
+var mission = 7895; // Сума для накопичення
+var period = 12; // К-сть місяців
 
-console.log("I am here!");
-alert("Hello to everyone!");
+console.log(typeof money); //Тип данних money
+console.log(typeof income); //Тип данних income
+console.log(typeof deposit); //Тип данних deposit
+console.log(addExpenses.length); // Довжина рядка addExpenses
+console.log("Період дорівнює " + period + " місяців");
+console.log("Ціль накопичення " + mission + " гривень");
+
+var toLower = addExpenses.toLowerCase(); // Змінну addExpenses до нижнього регістру й зберігаємо в змінну toLower
+var array = toLower.split(" , "); // Змінну toLower розбиваємо на масив
+console.log(array);
+
+var budgetDay = money / 30;
+console.log(budgetDay);
