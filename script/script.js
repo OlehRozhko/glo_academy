b61e91d4afa4d466129bc25a19fa0b19143762f4
Oleh Rozhko <oleh.rozhko5@gmail.com>
2021-10-20 21:53:40 +0300

    lesson03 is ready!

--- script/script.js
+++ script/script.js
@@ -1,9 +1,12 @@
+"use strict";
+
 /* jshint node: true */
+
 var money = 500; // Дохід за місяць
 var income = "Sport"; //Додатковий дохід
 var addExpenses = "Fitnes , Girls , Beer"; // Додаткові витрати
 var deposit = true;
-var mission = 7895; // Сума для накопичення
+var mission = 78958; // Сума для накопичення
 var period = 12; // К-сть місяців
 
 console.log(typeof money); //Тип данних money
@@ -17,5 +20,34 @@ var toLower = addExpenses.toLowerCase(); // Змінну addExpenses до ниж
 var array = toLower.split(" , "); // Змінну toLower розбиваємо на масив
 console.log(array);
 
-var budgetDay = money / 30;
-console.log(budgetDay);
+money = +prompt("Ваш місячний дохід?");
+console.log("money: ", money);
+addExpenses = prompt("Перечісліть додаткові витрати?", "Fitnes , Girls , Beer");
+console.log("addExpenses: ", addExpenses);
+deposit = confirm("Чи є у вас кредит у банку?");
+console.log("deposit: ", deposit);
+
+var expenses1, expenses2, amount1, amount2;
+expenses1 = prompt("Введіть обов'явозкову статтю витрат?");
+amount1 = prompt("Скільки це буде коштувати?");
+expenses2 = prompt("Введіть обов'явозкову статтю витрат?");
+amount2 = prompt("Скільки це буде коштувати?");
+
+var budgetMonth = money - amount1 - amount2;
+console.log("budgetMonth: ", budgetMonth);
+
+var getTarget = Math.ceil(mission / budgetMonth);
+console.log("getTarget: ", getTarget);
+
+var budgetDay = Math.floor(budgetMonth / 30);
+console.log("budgetDay: ", budgetDay);
+
+if (budgetDay > 1200) {
+  console.log("У вас високий рівень доходу");
+} else if (budgetDay > 600 && budgetDay < 1200) {
+  console.log("У вас середній рівень доходу");
+} else if (budgetDay > 0 && budgetDay < 600) {
+  console.log("На жаль у вас рівень доходу нижче середнього");
+} else if (budgetDay < 0) {
+  console.log("Щось пішло не так");
+}
