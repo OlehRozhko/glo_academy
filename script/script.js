0c0c25718a04f4f6f9ee83c79ca8b3bba10ec056
Oleh Rozhko <oleh.rozhko5@gmail.com>
2021-10-21 12:26:34 +0300

    lesson04 is ready

--- script/script.js
+++ script/script.js
@@ -9,44 +9,53 @@ var deposit = true;
 var mission = 78958; // Сума для накопичення
 var period = 12; // К-сть місяців
 
-console.log(typeof money); //Тип данних money
-console.log(typeof income); //Тип данних income
-console.log(typeof deposit); //Тип данних deposit
-console.log(addExpenses.length); // Довжина рядка addExpenses
-console.log("Період дорівнює " + period + " місяців");
-console.log("Ціль накопичення " + mission + " гривень");
+var showTypeOf = function (data) {
+  console.log(data, typeof data);
+};
+showTypeOf(money);
+showTypeOf(income);
+showTypeOf(deposit);
 
 var toLower = addExpenses.toLowerCase(); // Змінну addExpenses до нижнього регістру й зберігаємо в змінну toLower
 var array = toLower.split(" , "); // Змінну toLower розбиваємо на масив
 console.log(array);
 
-money = +prompt("Ваш місячний дохід?");
-console.log("money: ", money);
+money = +prompt("Ваш місячний дохід?", "10000");
 addExpenses = prompt("Перечісліть додаткові витрати?", "Fitnes , Girls , Beer");
-console.log("addExpenses: ", addExpenses);
 deposit = confirm("Чи є у вас кредит у банку?");
-console.log("deposit: ", deposit);
 
-var expenses1 = prompt("Введіть обов'явозкову статтю витрат?"),
-  amount1 = prompt("Скільки це буде коштувати?"),
-  expenses2 = prompt("Введіть обов'явозкову статтю витрат?"),
-  amount2 = prompt("Скільки це буде коштувати?");
+var expenses1 = prompt("Введіть обов'явозкову статтю витрат?", "Курси"),
+  amount1 = +prompt("Скільки це буде коштувати?", "4000"),
+  expenses2 = prompt("Введіть обов'явозкову статтю витрат?", "Водіння"),
+  amount2 = +prompt("Скільки це буде коштувати?", "1500");
 
-var budgetMonth = parseFloat(money) - parseFloat(amount1) - parseFloat(amount2);
-console.log("budgetMonth: ", budgetMonth);
+var getExpensesMonth = function () {
+  return amount1 + amount2;
+};
+console.log(getExpensesMonth());
 
-var getTarget = Math.ceil(mission / budgetMonth);
-console.log("getTarget: ", getTarget);
+var getAccumulatedMonth = function () {
+  return money - getExpensesMonth();
+};
+var accumulatedMonth = getAccumulatedMonth();
 
-var budgetDay = Math.floor(budgetMonth / 30);
+var getTargetMonth = function () {
+  return Math.ceil(mission / accumulatedMonth);
+};
+console.log(getTargetMonth());
+
+var budgetDay = Math.floor(accumulatedMonth / 30);
 console.log("budgetDay: ", budgetDay);
 
-if (budgetDay > 1200) {
-  console.log("У вас високий рівень доходу");
-} else if (budgetDay > 600 && budgetDay < 1200) {
-  console.log("У вас середній рівень доходу");
-} else if (budgetDay > 0 && budgetDay < 600) {
-  console.log("На жаль у вас рівень доходу нижче середнього");
-} else if (budgetDay < 0) {
-  console.log("Щось пішло не так");
-}
+var getStatusIncome = function () {
+  if (budgetDay > 1200) {
+    return "У вас високий рівень доходу";
+  } else if (budgetDay > 600 && budgetDay < 1200) {
+    return "У вас середній рівень доходу";
+  } else if (budgetDay > 0 && budgetDay < 600) {
+    return "На жаль у вас рівень доходу нижче середнього";
+  } else if (budgetDay < 0) {
+    return "Щось пішло не так";
+  }
+};
+console.log(getStatusIncome());
