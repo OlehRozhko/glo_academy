955ea3b84c16e8d3817c90000020b6651bb49b0b
Oleh Rozhko <oleh.rozhko5@gmail.com>
2021-10-27 15:04:47 +0300

    lesson07 is ready

--- script/script.js
+++ script/script.js
@@ -1,80 +1,85 @@
 "use strict";
 /* jshint node: true */
 
-let isNumber = (n) => {
-  console.log("n: ", n);
-  console.log(parseFloat(n));
-  console.log(isFinite(n));
+const isNumber = (n) => {
   return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
-var money, // Дохід за місяць
-  income = "Sport", //Додатковий дохід
-  addExpenses = "", // Додаткові витрати
-  deposit = true,
-  mission = 10000, // Сума для накопичення
-  period = 4; // К-сть місяців
+var money,
+  start = () => {
+    do {
+      money = prompt("Ваш місячний дохід?", "12000");
+    } while (!isNumber(money));
+  };
 
-do {
-  money = prompt("Ваш місячний дохід?");
-} while (!isNumber(money));
+start();
 
-addExpenses = prompt("Перечісліть додаткові витрати?", "Fitnes , Girls , Beer");
-deposit = confirm("Чи є у вас кредит у банку?");
-var showTypeOf = function (data) {
-  console.log(typeof data);
+let appData = {
+  income: {}, // додатковий дохід
+  addIncome: [],
+  expenses: {}, // список обов'язковий витрат
+  addExpenses: [], // рядок з перечислиними додатковими витратами
+  deposit: false, // наявність кредиту в банку
+  mission: 50000, // сума для накопичення
+  period: 5, // термін накопичення
+  budget: money, // дохід за місяць
+  budgetDay: 0, // бюджет на день
+  budgetMonth: 0, // бюджет на місяць
+  expensesMonth: 0, // місячні витрати
+  asking: function () {
+    appData.deposit = confirm("Чи є у вас кредит у банку?");
+    for (let i = 0; i < 4; i++) {
+      let first = prompt("Введіть обов'явозкову статтю витрат?");
+      let second = +prompt("Скільки це буде коштувати?");
+      appData.expenses[first] = second;
+    }
+    appData.addExpenses = prompt("Перечісліть додаткові витрати?");
+    appData.addExpenses.toLowerCase().split(" , ");
+  },
+  getExpensesMonth: function () {
+    // Функція повертає суму місячних витрат
+    for (let key in appData.expenses) {
+      appData.expensesMonth += appData.expenses[key];
+    }
+    return appData.expensesMonth;
+  },
+  getBudget: function () {
+    // Функція обчислює значенн budgetMonth and budgetDay
+    if (!appData.budget) {
+      appData.budget = 0;
+    }
+    appData.budgetMonth = money - appData.expensesMonth;
+    appData.budgetDay = appData.budgetMonth / 30;
+  },
+  getTargetMonth: function (myMission, budgetMonths) {
+    //Функція повертай період за скільки будуть накопичені гроші
+    return Math.ceil(myMission / budgetMonths);
+  },
+  getStatusIncome: function () {
+    // Функція вираховує,який рівень доходу
+    if (appData.budgetDay > 1200) {
+      return "У вас високий рівень доходу";
+    } else if (appData.budgetDay > 600 && appData.budgetDay < 1200) {
+      return "У вас середній рівень доходу";
+    } else if (appData.budgetDay > 0 && appData.budgetDay < 600) {
+      return "На жаль у вас рівень доходу нижче середнього";
+    } else if (appData.budgetDay < 0) {
+      return "Щось пішло не так";
+    }
+  },
 };
-showTypeOf(money);
-showTypeOf(income);
-showTypeOf(deposit);
+appData.asking();
+appData.getExpensesMonth();
+appData.getBudget();
 
-let expenses = [];
-
-console.log(addExpenses.toLowerCase().split(" , "));
-
-let getExpensesMonth = function () {
-  let sum = 0;
-
-  for (let i = 0; i < 4; i++) {
-    expenses[i] = prompt("Введіть обов'явозкову статтю витрат?");
-
-    sum += +prompt("Скільки це буде коштувати?");
-  }
-  console.log(sum);
-  return sum;
-};
-console.log(expenses.toLowerCase);
-let expensesAmount = getExpensesMonth();
-
-console.log("Місячні витрати: " + expensesAmount);
-
-var getAccumulatedMonth = function () {
-  return money - expensesAmount;
-};
-var accumulatedMonth = getAccumulatedMonth();
-
-var getTargetMonth = function (myMiss, budgetMonth) {
-  return Math.ceil(myMiss / budgetMonth);
-};
-
-var targetMonth = getTargetMonth(mission, accumulatedMonth);
+var targetMonth = appData.getTargetMonth();
 
 targetMonth > 0
   ? console.log("Ціла буде досягнути за " + targetMonth + " місяців")
   : console.log("Ціль не буде досягнута!");
 
-var budgetDay = Math.floor(accumulatedMonth / 30);
-console.log("budgetDay: ", budgetDay);
-
-var getStatusIncome = function () {
-  if (budgetDay > 1200) {
-    return "У вас високий рівень доходу";
-  } else if (budgetDay > 600 && budgetDay < 1200) {
-    return "У вас середній рівень доходу";
-  } else if (budgetDay > 0 && budgetDay < 600) {
-    return "На жаль у вас рівень доходу нижче середнього";
-  } else if (budgetDay < 0) {
-    return "Щось пішло не так";
-  }
-};
-console.log(getStatusIncome());
+console.log(appData.getStatusIncome());
+console.log("Наша програма включає в себе :");
+for (let data in appData) {
+  console.log(data, appData[data]);
+}
